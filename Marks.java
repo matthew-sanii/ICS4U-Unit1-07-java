b5a1f63d714455f755430660e81d97761b1a67af
Ubuntu <ubuntu@ip-172-31-90-22.ec2.internal>
2021-11-27 20:11:26 +0000

    edited Marks.java

--- Marks.java
+++ Marks.java
@@ -36,94 +36,48 @@ final class Marks {
     }
 
     /**
+    * Creates the marks for the generate program to use.
+    *
+    * @return the mark generated
+    */
+    public static int markCreate() {
+
+        final Random random = new Random();
+        final int average = 75;
+        final int deviate = 10;
+        final int mark = (int) Math.floor(random.nextGaussian() * deviate + average);
+        return mark;
+    }
+
+    /**
     * The generateMarks function.
     *
     * @param arrayOfStudents the list for students
     * @param arrayOfAssignments the list of assignments
+    * @param name the name of the file you want to create
     * @return the combined array of the two lists
-    * @throws IoException when error occurs
+    * @throws IOException when error occurs
+    *
     */
     public static String[][] generateMarks(final String[] arrayOfStudents,
-                                       final String[] arrayOfAssignments) throws IOException {
+                                       final String[] arrayOfAssignments,
+                                       final String name) throws IOException {
 
-        final int average = 75;
-        final int deviate = 10;
         final int numStudents = arrayOfStudents.length;
         final int numAssignments = arrayOfAssignments.length;
-        // add 1 to both values to take into account the student names and assignment names.
         final String[][] markArray = new String[numStudents + 1][numAssignments + 1];
-        // put a blank value in the first XX of the array
         markArray[0][0] = "  ";
         for (int row = 1; row <= numStudents; row++) {
             markArray[row][0] = arrayOfStudents[row - 1];
+            markArray[0][row] = arrayOfAssignments[row - 1];
         }
-        for (int collumn = 1; collumn <= numAssignments; collumn++) {
-            markArray[0][collumn] = arrayOfAssignments[collumn - 1];
-        }
-        final Random random = new Random();
         for (int vertical = 1; vertical <= numStudents; vertical++) {
             for (int horizontal = 1; horizontal <= numStudents; horizontal++) {
-                final int mark = (int) Math.floor(random.nextGaussian() * deviate + average);
-                final String marks = String.valueOf(mark);
-                markArray[vertical][horizontal] = marks;
+                final int grade = markCreate();
+                markArray[vertical][horizontal] = String.valueOf(grade);
             }
         }
-
-        File file = new File(fileName);
-        FileWriter fw = new FileWriter(file);
-        BufferedWriter bw = new BufferedWriter(fw);
-
-        for (int y = 0; y <= numStudents; y++) {
-           String fileline = "";
-           for (int x = 0; x <= numAssignments; x++) {
-               if (x == 0 && y == 0) {
-                  fileline += "  , ";
-               }
-               else {
-                  if (x == numAssignments) {
-                      fileline += String.valueOf(markArray[y][x]);
-                  }
-                  else {
-                      fileline += String.valueOf(markArray[y][x]) + ", ";
-                  }
-               }
-           }
-           bw.write(fileline);
-           bw.newLine();
-        }
-        //Close the csv file.
-        bw.close();
-        fw.close();
-
-        return markArray;
-    }
-    /**
-    * The printArray function.
-    *
-    * @param arrayList 2d array taken from other function.
-    *
-    */
-
-    public static void printArray(String[][] arrayList) {
-        final String blank = " ";
-        for (int roow = 0; roow < arrayList.length; roow++) {
-            for (int column = 0; column < arrayList[0].length; column++) {
-                System.out.print(arrayList[roow][column] + blank);
-            }
-            System.out.println(blank);
-        }
-    }
-
-    /**
-    * The starting main() function.
-    *
-    * @param args Name of file containing a string of numbers
-    */
-    public static void main(final String[] args) throws IOException {
-        final ArrayList<String> listOfStudents = new ArrayList<String>();
-        final ArrayList<String> listOfAssignments = new ArrayList<String>();
-        final Charset charset = Charset.forName("UTF-8");
-        final String fileName = "marks.csv";
+        final String fileName = name;
         try {
             final File myObj = new File(fileName);
             if (myObj.createNewFile()) {
@@ -131,32 +85,79 @@ final class Marks {
             } else {
                 System.out.println("File already exists.");
             }
-        } catch (IOException e) {
-                System.out.println("An error occurred trying to create the marks.csv file.");
-                e.printStackTrace();
+        } catch (IOException errorCode) {
+            System.out.println("Error creating file.");
         }
+        final FileWriter write = new FileWriter(fileName);
+        final BufferedWriter buffWrite = new BufferedWriter(write);
 
-        if (!Files.exists(Paths.get(args[0])) || !Files.exists(Paths.get(args[1]))) {
-            System.err.println("Exiting as the necessary input files do not exist: " + args[0] + " or " + args[1]);
-            System.exit(0);
+        for (int y = 0; y <= numStudents; y++) {
+            String fileline = "";
+            for (int x = 0; x <= numAssignments; x++) {
+                if (x == 0 && y == 0) {
+                    fileline += "  , ";
+                } else {
+                    if (x == numAssignments) {
+                        fileline += String.valueOf(markArray[y][x]);
+                    } else {
+                        fileline += String.valueOf(markArray[y][x]) + ", ";
+                    }
+                }
+            }
+            buffWrite.write(fileline);
+            buffWrite.newLine();
         }
-        final Path studentFilePath = Paths.get(args[0]);
-        final Path assignmentFilePath = Paths.get(args[1]);
+        buffWrite.close();
+        write.close();
 
-        File file1 = new File(args[0]);
-        File file2 = new File(args[1]);
-        if (file1.length() == 0 || file2.length() == 0) {
+        return markArray;
+    }
+
+    /**
+    * The fileCheck function.
+    *
+    * @param value1 int value of first file size
+    * @param value2 int value of second file size
+    */
+
+    public static void fileCheck(long value1, long value2) {
+        if (value1 == 0 || value2 == 0) {
             System.out.println("One of the input files is empty. Both files must contain data.");
             System.out.println("Please check files.");
             System.exit(0);
         }
+    }
 
-         try (BufferedReader readerStudent = Files.newBufferedReader(
-                                      studentFilePath, charset)) {
-             String lineStudent = "hi";
-             while ((lineStudent = readerStudent.readLine()) != null) {
-                 try {
-                     listOfStudents.add(lineStudent);
+    /**
+    * The starting main() function.
+    *
+    * @param args Name of file containing a string of numbers
+    * @throws IOException when error occurs
+    */
+
+    public static void main(final String[] args) throws IOException {
+        final ArrayList<String> listOfStudents = new ArrayList<String>();
+        final ArrayList<String> listOfAssignments = new ArrayList<String>();
+        final Charset charset = Charset.forName("UTF-8");
+        final String namedFile = "marks.csv";
+        if (!Files.exists(Paths.get(args[0])) || !Files.exists(Paths.get(args[1]))) {
+            System.err.println("Exiting as the necessary input files do not exist: "
+                + args[0] + " or " + args[1]);
+            System.exit(0);
+        }
+        final Path studentFilePath = Paths.get(args[0]);
+        final Path assignmentFilePath = Paths.get(args[1]);
+        final File file1 = new File(args[0]);
+        final File file2 = new File(args[1]);
+        final long fileSize1 = file1.length();
+        final long fileSize2 = file2.length();
+        fileCheck(fileSize1, fileSize2);
+        try (BufferedReader readerStudent = Files.newBufferedReader(
+                                     studentFilePath, charset)) {
+            String lineStudent = "hi";
+            while ((lineStudent = readerStudent.readLine()) != null) {
+                try {
+                    listOfStudents.add(lineStudent);
                 } catch (ArrayIndexOutOfBoundsException errorCode) {
                     lineStudent = null;
                 }
@@ -177,7 +178,6 @@ final class Marks {
         final int nameSize = listOfStudents.size();
         final String[] arrayOfAssignments = listOfAssignments.toArray(new String[assignmentSize]);
         final String[] arrayOfStudents = listOfStudents.toArray(new String[nameSize]);
-        final String[][] marks;
-        generateMarks(arrayOfStudents, arrayOfAssignments);
+        generateMarks(arrayOfStudents, arrayOfAssignments, namedFile);
     }
 }
