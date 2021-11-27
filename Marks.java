febadf6fc49c41b10c1b9c38f97c486aa6507c8e
Ubuntu <ubuntu@ip-172-31-90-22.ec2.internal>
2021-11-27 00:46:22 +0000

    edited Marks.java

--- Marks.java
+++ Marks.java
@@ -7,15 +7,18 @@
 * @since   2021-26-11
 */
 
+import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
+
 
 /**
 * This is the marks program.
@@ -44,35 +47,52 @@ final class Marks {
     public static String[][] generateMarks(final String[] arrayOfStudents,
                                        final String[] arrayOfAssignments) {
 
-        int numStudents = arrayOfStudents.length;
-        int numAssignments = arrayOfAssignments.length;
+        final int average = 75;
+        final int deviate = 10;
+        final int numStudents = arrayOfStudents.length;
+        final int numAssignments = arrayOfAssignments.length;
         // add 1 to both values to take into account the student names and assignment names.
-        String[][] markArray = new String[numStudents + 1][numAssignments + 1];
+        final String[][] markArray = new String[numStudents + 1][numAssignments + 1];
         for (int row = 1; row <= numStudents; row++) {
             markArray[row][0] = arrayOfStudents[row - 1];
         }
         for (int collumn = 1; collumn <= numAssignments; collumn++) {
             markArray[0][collumn] = arrayOfAssignments[collumn - 1];
         }
-        Random random = new Random();
+        final Random random = new Random();
         for (int vertical = 1; vertical <= numStudents; vertical++) {
             for (int horizontal = 1; horizontal <= numStudents; horizontal++) {
-                int mark = (int)Math.floor(random.nextGaussian()*10+75);
-                String marks = String.valueOf(mark);
+                final int mark = (int) Math.floor(random.nextGaussian() * deviate + average);
+                final String marks = String.valueOf(mark);
                 markArray[vertical][horizontal] = marks;
             }
         }
         markArray[0][0] = "   ";
         printArray(markArray);
+        BufferedWriter br = new BufferedWriter(new FileWriter("marks.csv"));
+        StringBuilder sb = new StringBuilder();
+        for (String[] element : markArray) {
+            sb.append(element);
+            sb.append(",");
+        }
+        br.write(sb);
+        br.close();
         return markArray;
     }
+    /**
+    * The printArray function.
+    *
+    * @param arrayList 2d array taken from other function.
+    *
+    */
 
     private static void printArray(String[][] arrayList) {
+        final String blank = " ";
         for (int roow = 0; roow < arrayList.length; roow++) {
             for (int column = 0; column < arrayList[0].length; column++) {
-                System.out.print(arrayList[roow][column] + " ");
+                System.out.print(arrayList[roow][column] + blank);
             }
-            System.out.println(" ");
+            System.out.println(blank);
         }
     }
 
@@ -86,12 +106,20 @@ final class Marks {
         final ArrayList<String> listOfAssignments = new ArrayList<String>();
         final Charset charset = Charset.forName("UTF-8");
 
-        if (!Files.exists(Paths.get(args[0]))) {
-            System.err.println("Exiting as file does not exist: " + args[0]);
-            System.exit(0);
+        try {
+            File myObj = new File("marks.csv");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+        } else {
+                System.out.println("File already exists.");
         }
-        if (!Files.exists(Paths.get(args[1]))) {
-            System.err.println("Exiting as file does not exist: " + args[1]);
+      } catch (IOException e) {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+      }
+
+        if (!Files.exists(Paths.get(args[0])) || !Files.exists(Paths.get(args[1]))) {
+            System.err.println("Exiting as a file does not exist: " + args[0] + " or " + args[1]);
             System.exit(0);
         }
         final Path studentFilePath = Paths.get(args[0]);
@@ -115,9 +143,9 @@ final class Marks {
             while ((lineAssignment = readerAssignment.readLine()) != null) {
                 listOfAssignments.add(lineAssignment);
             }
-            } catch (IOException errorCode) {
-                System.err.println(errorCode);
-            }
+        } catch (IOException errorCode) {
+            System.err.println(errorCode);
+        }
         final int assignmentSize = listOfAssignments.size();
         final int nameSize = listOfStudents.size();
         final String[] arrayOfAssignments = listOfAssignments.toArray(new String[assignmentSize]);
